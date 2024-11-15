61fe7c1f015fe75bf2e5085049e815007c6b1a4e
mayur-uniyal <42968384+mayur-uniyal@users.noreply.github.com>
2024-11-15 00:57:43 +0000

    Conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+/*let score = "33as"
+let score1 = null
+let score2 = ""
+//let score3 = undefinned
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+console.log(typeof(score1));
+console.log(score1);
+console.log(score2);
+console.log(typeof(score3)); */
+
+let sample = 888
+console.log(sample);
+console.log(typeof(sample));
+
+let og = false
+let valueNumebr = Number(og)
+console.log(valueNumebr)
+console.log(typeof(valueNumebr));
+
+//"1234" => 1234
+//"1234ab" => NaN{{Not a number}}
+//true => 1; false => 0
+
+//And this is how we can covert variable from one form to another ex: Number -> String, Number -> Boolean etc.
